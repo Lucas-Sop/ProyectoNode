61ca9b6734ff1257ec6f648412fcfe0c56013f35
lgomez.sop <lgomez.sop@gmail.com>
2023-08-21 18:51:14 -0300

    Fix line endings

---
+++ app.js
@@ -0,0 +1,15 @@
+const evenOrOdd = require('num-is-even-or-odd');
+
+
+console.log(evenOrOdd(25))
+console.log(evenOrOdd(90))
+
+console.log('este numero es del tipo', evenOrOdd(87))
+console.log('hola')
+console.log("Buenas noches como estas")
+// console.log("hola mundo")
+// function saludo(nombre){
+//     console.log(`Hola buenos dias ${nombre}`)
+// }
+
+// saludo('manuel')
